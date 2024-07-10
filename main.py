6eb1c1024c324566729903f4920f2433c653e1fd
4andrey <andrey.1998k@icloud.com>
2024-07-10 21:08:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#1st program
+print( 9 ** 0.5 * 5 )
+
+
+#2nd program
+print( 9.99 > 9.98 and 1000 != 1000.1 )
+
+
+#3rd program
+a = 1234 % 1000 // 10
+b = 5678 % 1000 //10
+c = a + b
+print(c)
+
+
+#4th program
+a = 13.42
+b = 42.13
+ai = int(a)
+bi = int(b)
+af = int((a - ai) * 100 )
+bf = int((b-bi) * 100 )
+print (ai == bf or bi == af)
